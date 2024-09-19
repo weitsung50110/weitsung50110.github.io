48dc54be7bbe607b7970a3b437f0c32a2fee7747
weitsung50110 <a1233z51ab10@gmail.com>
2024-09-19 11:00:25 +0800

    comment out the copy button

--- js/post.js
+++ js/post.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const codeBlocks = document.querySelectorAll('pre code');
-    codeBlocks.forEach(block => {
-        const button = document.createElement('button');
-        button.textContent = 'Copy';
-        button.className = 'copy-button';
-        block.parentElement.style.position = 'relative';
-        block.parentElement.appendChild(button);
+// document.addEventListener('DOMContentLoaded', function() {
+//     const codeBlocks = document.querySelectorAll('pre code');
+//     codeBlocks.forEach(block => {
+//         const button = document.createElement('button');
+//         button.textContent = 'Copy';
+//         button.className = 'copy-button';
+//         block.parentElement.style.position = 'relative';
+//         block.parentElement.appendChild(button);
 
-        button.addEventListener('click', () => {
-            const range = document.createRange();
-            range.selectNode(block);
-            window.getSelection().removeAllRanges();
-            window.getSelection().addRange(range);
-            document.execCommand('copy');
-            window.getSelection().removeAllRanges();
-            // alert('Code copied to clipboard!');
-        });
-    });
-});
+//         button.addEventListener('click', () => {
+//             const range = document.createRange();
+//             range.selectNode(block);
+//             window.getSelection().removeAllRanges();
+//             window.getSelection().addRange(range);
+//             document.execCommand('copy');
+//             window.getSelection().removeAllRanges();
+//             // alert('Code copied to clipboard!');
+//         });
+//     });
+// });
 
 document.addEventListener('DOMContentLoaded', function() {
   const tables = document.querySelectorAll('article table');
